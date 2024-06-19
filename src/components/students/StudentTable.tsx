2be38b0ffb757d567daf88105c15f0a378d0e695
MrHumble24 <abdulboriyweb@gmail.com>
2024-06-18 21:50:22 -0700

    feat: added filter by classes to students

--- src/components/students/StudentTable.tsx
+++ src/components/students/StudentTable.tsx
@@ -1,6 +1,8 @@
 // StudentsTable.tsx
 import {
   Box,
+  Select,
+  Spinner,
   Table,
   Tbody,
   Th,
@@ -12,6 +14,7 @@ import React, { useState } from "react";
 import { Students } from "../../types/types";
 import EditStudentModal from "./EditStudentModal";
 import TeacherTableRow from "./StudentsTableRow";
+import { useClasses } from "../../hooks/useClasses";
 
 type StudentTableProps = {
   students?: Students[];
@@ -22,6 +25,22 @@ type StudentTableProps = {
 const StudentsTable: React.FC<StudentTableProps> = ({ students, onDelete }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [selectedUser, setSelectedUser] = useState<Students | null>(null);
+  const { data: classes, isLoading: classesLoading } = useClasses();
+  const [studentList, setStudentList] = useState<Students[] | undefined>(
+    students
+  );
+
+  const handleFilterStudents = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedClass = e.target.value;
+    if (selectedClass === "all") {
+      setStudentList(students);
+    } else {
+      const filteredStudents = students?.filter(
+        (s) => s.class == Number(selectedClass)
+      );
+      setStudentList(filteredStudents);
+    }
+  };
 
   const handleEdit = (user: Students) => {
     setSelectedUser(user);
@@ -29,39 +48,52 @@ const StudentsTable: React.FC<StudentTableProps> = ({ students, onDelete }) => {
   };
 
   return (
-    <Box overflow={"scroll"}>
-      <Table size={"sm"} variant='simple'>
-        <Thead>
-          <Tr>
-            <Th>First Name</Th>
-            <Th>Last Name</Th>
-            <Th>Username</Th>
-            <Th>Password</Th>
-            <Th>Class</Th>
-            <Th>Tasks</Th>
-            <Th>Exams</Th>
-            <Th>Books</Th>
-            <Th>Actions</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {students?.map((user) => (
-            <TeacherTableRow
-              key={user.id}
-              user={user}
-              onEdit={handleEdit}
-              onDelete={onDelete}
-            />
+    <>
+      <Box my={4}>
+        {classesLoading && <Spinner />}
+        <Select onChange={handleFilterStudents} maxW={200} size='sm'>
+          <option value='all'>Filter by All Classes</option>
+          {classes?.map((c) => (
+            <option key={c.id} value={c.id}>
+              {c.name}
+            </option>
           ))}
-        </Tbody>
-      </Table>
+        </Select>
+      </Box>
+      <Box overflow={"scroll"}>
+        <Table size={"sm"} variant='simple'>
+          <Thead>
+            <Tr>
+              <Th>First Name</Th>
+              <Th>Last Name</Th>
+              <Th>Username</Th>
+              <Th>Password</Th>
+              <Th>Class</Th>
+              <Th>Tasks</Th>
+              <Th>Exams</Th>
+              <Th>Books</Th>
+              <Th>Actions</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {studentList?.map((user) => (
+              <TeacherTableRow
+                key={user.id}
+                user={user}
+                onEdit={handleEdit}
+                onDelete={onDelete}
+              />
+            ))}
+          </Tbody>
+        </Table>
 
-      <EditStudentModal
-        isOpen={isOpen}
-        onClose={onClose}
-        defaultValues={selectedUser}
-      />
-    </Box>
+        <EditStudentModal
+          isOpen={isOpen}
+          onClose={onClose}
+          defaultValues={selectedUser}
+        />
+      </Box>
+    </>
   );
 };
 
